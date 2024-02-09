5392abde18c5b5d8efaee0454e071879f8335ffd
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-02-09 14:24:07 +0100

    Solve json.load string issues

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -49,6 +49,16 @@ class FileStorage():
             with open(self.__file_path, 'r') as f:
                 py_obj = json.load(f)
                 for k,v in py_obj.items():
+                    '''
+                    For each of the key in the JSON file, recreate
+                    the BaseModel. **v indicates that we are passing
+                    each of the value (BasedModel.__dict__) that we
+                    previously saved as keyword args. If you forget
+                    to add the ** it will import it all as string
+                    which will affect the self.created_at and
+                    self.updated_at attribute by not allowing it
+                    to convert into datetime appropriately.
+                    '''
                     self.__objects[k] = BaseModel(**v)
             return py_obj
         except Exception as e:
